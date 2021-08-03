6dc92f84065acd353f0b93cbaea712b943716d23
FilipNilsen <32814262+FilipNilsen@users.noreply.github.com>
2021-08-03 17:24:44 +0200

    Added article about abandoned road

---
+++ gammel_e18/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../head.php'; ?>
+<body>
+<?php include '../titlebar.php'; ?>
+<h1>Forlatt gammel del av E18</h1>
+<p>Denne delen av E18 har blitt forlatt og stengt etter at ny motorvei har erstattet den.
+Veien går gjennom Dørdal og ligger ikke langt unna E18 Kryss 53 Langrønningkrysset.
+På et punkt går veien i bro over en grussti som heter Gongveien.
+<a href="https://www.openstreetmap.org/#map=18/58.96337/9.47787">OpenStreetMap</a>.<br>
+<a href="https://www.norgeibilder.no/?x=182410&y=6549076&level=14&utm=33&projects=&layers=&plannedOmlop=0&plannedGeovekst=0">Norge i Bilder</a><br>
+<a href="https://www.openstreetmap.org/?mlat=58.96220&mlon=9.47339#map=17/58.96220/9.47339">Man kan parkere her ved kretshuset</a>.
+</p>
+<p>
+<img src="images/IMG_20210717_215345.jpg" alt="" /><br><img src="images/IMG_20210717_215450.jpg" alt="" /><br><img src="images/IMG_20210717_215453.jpg" alt="" /><br><img src="images/IMG_20210717_215613.jpg" alt="" /><br><img src="images/IMG_20210717_215656.jpg" alt="" /><br><img src="images/IMG_20210717_215733.jpg" alt="" /><br><img src="images/IMG_20210717_215747.jpg" alt="" /><br><img src="images/IMG_20210717_215754.jpg" alt="" /><br><img src="images/IMG_20210717_215755.jpg" alt="" /><br><img src="images/IMG_20210717_215821.jpg" alt="" /><br><img src="images/IMG_20210717_215824.jpg" alt="" /><br><img src="images/IMG_20210717_215853.jpg" alt="" /><br><img src="images/IMG_20210717_215909.jpg" alt="" /><br><img src="images/IMG_20210717_220021.jpg" alt="" /><br><img src="images/IMG_20210717_220036.jpg" alt="" /><br><img src="images/IMG_20210717_220141.jpg" alt="" /><br><img src="images/IMG_20210717_220144.jpg" alt="" /><br><img src="images/IMG_20210717_220151.jpg" alt="" /><br><img src="images/IMG_20210717_220243.jpg" alt="" /><br><img src="images/IMG_20210717_220256.jpg" alt="" /><br><img src="images/IMG_20210717_220323.jpg" alt="" /><br><img src="images/IMG_20210717_220418.jpg" alt="" /><br><img src="images/IMG_20210717_220509.jpg" alt="" /><br><img src="images/IMG_20210717_220552.jpg" alt="" /><br><img src="images/IMG_20210717_220605.jpg" alt="" /><br><img src="images/IMG_20210717_220706.jpg" alt="" /><br><img src="images/IMG_20210717_220722.jpg" alt="" /><br><img src="images/IMG_20210717_220805.jpg" alt="" /><br><img src="images/IMG_20210717_220819.jpg" alt="" /><br><img src="images/IMG_20210717_220829.jpg" alt="" /><br><img src="images/IMG_20210717_220848.jpg" alt="" /><br><img src="images/IMG_20210717_220854.jpg" alt="" /><br><img src="images/IMG_20210717_220919.jpg" alt="" /><br><img src="images/IMG_20210717_221231.jpg" alt="" /><br><img src="images/IMG_20210717_221241.jpg" alt="" /><br><img src="images/IMG_20210717_221307.jpg" alt="" /><br><img src="images/IMG_20210717_221309.jpg" alt="" /><br><img src="images/IMG_20210717_221323.jpg" alt="" /><br><img src="images/IMG_20210717_221331.jpg" alt="" /><br>
+</p>+
